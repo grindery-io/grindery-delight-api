8a72cf809e3d24ec456c4794e962a2b1403043af
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-03-15 19:42:49 -0300

    lint

--- src/routes/staking.js
+++ src/routes/staking.js
@@ -7,38 +7,42 @@ const router = express.Router();
 
 /* This is a post request to the staking collection. */
 router.post('/', isRequired, async (req, res) => {
-    let collection = db.collection('staking');
-    let newDocument = req.body;
-    newDocument.date = new Date();
-    newDocument.userId = res.locals.userId;
-    let result = await collection.insertOne(newDocument);
-    res.send(result).status(201);
+  let collection = db.collection('staking');
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  newDocument.userId = res.locals.userId;
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(201);
 });
 
 /* This is a post request to the staking collection. */
-router.post('/modify/chainId/:chainId/amount/:amount', isRequired, async (req, res) => {
+router.post(
+  '/modify/chainId/:chainId/amount/:amount',
+  isRequired,
+  async (req, res) => {
     let collection = db.collection('staking');
     let result = await collection.updateOne(
-        {
-            userId: res.locals.userId,
-            chainId: req.params.chainId
-        },
-        {
-            $set: {amount: req.params.amount}
-        }
+      {
+        userId: res.locals.userId,
+        chainId: req.params.chainId,
+      },
+      {
+        $set: { amount: req.params.amount },
+      }
     );
     res.send(result).status(201);
-});
+  }
+);
 
 /* This is a delete request to the staking collection. */
 router.delete('/chainId/:chainId', isRequired, async (req, res) => {
-    const query = {
-        chainId: req.params.chainId,
-        userId: res.locals.userId
-     };
-    const collection = db.collection('staking');
-    let result = await collection.deleteOne(query);
-    res.send(result).status(200);
+  const query = {
+    chainId: req.params.chainId,
+    userId: res.locals.userId,
+  };
+  const collection = db.collection('staking');
+  let result = await collection.deleteOne(query);
+  res.send(result).status(200);
 });
 
 export default router;
