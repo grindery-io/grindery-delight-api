63d89e0569913ca2ce808aa2bec20c07b43dfc20
Thomas Coratger <thomas.coratger@gmail.com>
2023-03-17 12:24:26 +0100

    route get all wallets with arbitrary filter

--- src/routes/liquidity-wallets.js
+++ src/routes/liquidity-wallets.js
@@ -2,7 +2,10 @@ import express from 'express';
 import db from '../db/conn.js';
 import isRequired from '../utils/auth-utils.js';
 import { ObjectId } from 'mongodb';
-import { liquidityWalletValidator } from '../validators/liquidity-wallets.validator.js';
+import {
+  createLiquidityWalletValidator,
+  getLiquidityWalletValidator,
+} from '../validators/liquidity-wallets.validator.js';
 import { validateResult } from '../utils/validators-utils.js';
 
 const router = express.Router();
@@ -10,7 +13,7 @@ const router = express.Router();
 /* Creating a new wallet for the user. */
 router.post(
   '/create-wallet',
-  liquidityWalletValidator,
+  createLiquidityWalletValidator,
   isRequired,
   async (req, res) => {
     let collection = db.collection('liquidity-wallets');
@@ -72,11 +75,21 @@ router.put(
   }
 );
 
-// router.get('/', isRequired, async (req, res) => {
-//   let collection = db.collection('liquidity-wallets');
-//   let result = await collection.findOne({
-//     _id: new ObjectId(req.params.idOffer),
-//   });
-// });
+router.get('/', getLiquidityWalletValidator, isRequired, async (req, res) => {
+  const validator = validateResult(req, res);
+  if (validator.length) {
+    return res.send(validator).status(400);
+  }
+  const result = (
+    await db.collection('liquidity-wallets').find(req.body).toArray()
+  ).filter((e) => e.userId === res.locals.userId);
+  if (result.length !== 0) {
+    res.send(result).status(200);
+  } else {
+    res.status(404).send({
+      message: 'Not Found',
+    });
+  }
+});
 
 export default router;
