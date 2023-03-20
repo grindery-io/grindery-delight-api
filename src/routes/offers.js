9ec31d17be6dd8f58e3314a40da9aa7fcb900082
Thomas Coratger <thomas.coratger@gmail.com>
2023-03-20 20:02:28 +0100

    new trade in offers

--- src/routes/offers.js
+++ src/routes/offers.js
@@ -6,6 +6,7 @@ import {
   getOfferByIdValidator,
   deleteOfferValidator,
   updateOfferValidator,
+  addTradeOfferValidator,
 } from '../validators/offers.validator.js';
 import { validateResult } from '../utils/validators-utils.js';
 import { ObjectId } from 'mongodb';
@@ -23,6 +24,7 @@ router.post('/', createOfferValidator, isRequired, async (req, res) => {
   let newDocument = req.body;
   newDocument.date = new Date();
   newDocument.userId = res.locals.userId;
+  newDocument.trades = new Map();
   let result = await collection.insertOne(newDocument);
   res.send(result).status(201);
 });
@@ -88,6 +90,49 @@ router.delete(
     if (offer?.userId === res.locals.userId) {
       let result = await collection.deleteOne(query);
       res.status(200).send(result);
+    } else {
+      res.status(404).send({
+        msg: 'Not Found',
+      });
+    }
+  }
+);
+
+// PUT
+
+router.put(
+  '/new-trade',
+  addTradeOfferValidator,
+  isRequired,
+  async (req, res) => {
+    const validator = validateResult(req, res);
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const collection = db.collection('offers');
+    const offer = await collection.findOne({
+      offerId: req.body.offerId,
+      userId: res.locals.userId,
+    });
+    if (offer && !(req.body.tradeId in offer.trades)) {
+      res.status(200).send(
+        await collection.updateOne(
+          offer,
+          {
+            $set: {
+              [`trades.${req.body.tradeId}`]: {
+                amountGRT: req.body.amountGRT,
+                user: req.body.user,
+                destAddr: req.body.destAddr,
+                amountToken: req.body.amountToken,
+                tradeId: req.body.tradeId,
+                isComplete: false,
+              },
+            },
+          },
+          { upsert: false }
+        )
+      );
     } else {
       res.status(404).send({
         msg: 'Not Found',
--- src/validators/offers.validator.js
+++ src/validators/offers.validator.js
@@ -31,6 +31,11 @@ export const createOfferValidator = [
     .withMessage('must be string value')
     .notEmpty()
     .withMessage('must not be empty'),
+  body('offerId')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('must not be empty'),
   body('isActive')
     .isBoolean()
     .withMessage('must be boolean value')
@@ -48,4 +53,37 @@ export const deleteOfferValidator = [
 
 export const updateOfferValidator = [
   param('idOffer').notEmpty().withMessage('must not be empty'),
+];
+
+export const addTradeOfferValidator = [
+  body('amountGRT')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('should not be empty'),
+  body('offerId')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('should not be empty'),
+  body('destAddr')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('should not be empty'),
+  body('user')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('should not be empty'),
+  body('amountToken')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('should not be empty'),
+  body('tradeId')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('should not be empty'),
 ];
