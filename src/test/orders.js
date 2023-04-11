25cab8418117960a6169d0b96a9e297286f99521
Thomas Coratger <thomas.coratger@gmail.com>
2023-04-11 18:24:47 +0200

    tests for get orders liquidity provider

--- src/test/orders.js
+++ src/test/orders.js
@@ -10,6 +10,7 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 const collection = db.collection('orders');
+const offerCollection = db.collection('offers');
 
 const orderId = 'myOrderId';
 
@@ -22,6 +23,29 @@ const order = {
   orderId: orderId,
   amountTokenOffer: '5433',
   hash: 'myhash',
+};
+
+const offerId =
+  '0x02689c291c6d392ab9c02fc2a459a08cc46cc816b77cec928c86109d37ed2843';
+
+const offer = {
+  chainId: '97',
+  min: '0.02',
+  max: '1',
+  tokenId: '45',
+  token: 'BNB',
+  tokenAddress: '0x0',
+  hash: '0x56ee9a0e1063631dbdb5f2b8c6946aecf9a765a9470f023e3a8afb8fbf86d7a4',
+  exchangeRate: '1',
+  exchangeToken: 'ETH',
+  exchangeChainId: '5',
+  estimatedTime: '123',
+  provider: '0x795beefD41337BB83903788949c8AC2D559A44a3',
+  offerId: offerId,
+  isActive: true,
+  title: '',
+  image: '',
+  amount: '',
 };
 
 describe('Orders route', () => {
@@ -620,6 +644,276 @@ describe('Orders route', () => {
       chai.expect(res).to.have.status(200);
       chai.expect(res.body).to.be.an('object');
       chai.expect(res.body).to.be.empty;
+    });
+  });
+
+  describe('GET by MongoDbId', () => {
+    it('Should return 403 if no token is provided', async function () {
+      const res = await chai
+        .request(app)
+        .get('/orders/id')
+        .query({ id: 'mongoDbId' });
+      chai.expect(res).to.have.status(403);
+    });
+
+    it('Should return the order with the proper MongoDB id', async function () {
+      const createResponse = await chai
+        .request(app)
+        .post('/orders')
+        .set('Authorization', `Bearer ${mockedToken}`)
+        .send(order);
+      chai.expect(createResponse).to.have.status(200);
+
+      const res = await chai
+        .request(app)
+        .get('/orders/id')
+        .set({ Authorization: `Bearer ${mockedToken}` })
+        .query({ id: createResponse.body.insertedId });
+
+      delete res.body._id;
+      delete res.body.date;
+      delete res.body.userId;
+      delete res.body.isComplete;
+
+      chai.expect(res).to.have.status(200);
+      chai.expect(res.body).to.be.an('object');
+      chai.expect(res.body).to.deep.equal(order);
+
+      const deleteResponse = await chai
+        .request(app)
+        .delete(`/orders/${orderId}`)
+        .set('Authorization', `Bearer ${mockedToken}`);
+      chai.expect(deleteResponse).to.have.status(200);
+    });
+
+    it('Should return the order with the proper userId', async function () {
+      const createResponse = await chai
+        .request(app)
+        .post('/orders')
+        .set('Authorization', `Bearer ${mockedToken}`)
+        .send(order);
+      chai.expect(createResponse).to.have.status(200);
+
+      const userId = (
+        await collection.findOne({
+          _id: new ObjectId(createResponse.body.insertedId),
+        })
+      ).userId;
+
+      const res = await chai
+        .request(app)
+        .get('/orders/id')
+        .set({ Authorization: `Bearer ${mockedToken}` })
+        .query({ id: createResponse.body.insertedId });
+
+      chai.expect(res).to.have.status(200);
+      chai.expect(res.body.userId).to.equal(userId);
+
+      const deleteResponse = await chai
+        .request(app)
+        .delete(`/orders/${orderId}`)
+        .set('Authorization', `Bearer ${mockedToken}`);
+      chai.expect(deleteResponse).to.have.status(200);
+    });
+
+    it('Should return an empty object if MongoDB id doesnt exist', async function () {
+      const res = await chai
+        .request(app)
+        .get('/orders/id')
+        .set({ Authorization: `Bearer ${mockedToken}` })
+        .query({ id: '111111111111111111111111' });
+
+      chai.expect(res).to.have.status(200);
+      chai.expect(res.body).to.be.an('object').that.is.empty;
+    });
+  });
+
+  describe('GET by liquidity provider', () => {
+    it('Should return 403 if no token is provided', async function () {
+      const res = await chai.request(app).get('/orders/liquidity-provider');
+      chai.expect(res).to.have.status(403);
+    });
+
+    it('Should show only orders with existing offerId in the offers collection', async function () {
+      this.timeout(50000);
+      const customOffer = { ...offer };
+      const customOrder = { ...order };
+      const nbrOffersOrders = 1;
+      let customOfferIds = [];
+      for (let i = 0; i < nbrOffersOrders; i++) {
+        customOffer.offerId = `offerId-number${i}`;
+        // isActive est true pour les i pairs, false pour les i impairs
+        customOffer.isActive = i % 2 === 0;
+
+        customOfferIds.push(customOffer.offerId);
+
+        const newOffer = await chai
+          .request(app)
+          .post('/offers')
+          .set('Authorization', `Bearer ${mockedToken}`)
+          .send(customOffer);
+        chai.expect(newOffer).to.have.status(200);
+
+        customOrder.orderId = `orderId-number${i}`;
+        customOrder.offerId = customOffer.offerId;
+
+        const newOrder = await chai
+          .request(app)
+          .post('/orders')
+          .set('Authorization', `Bearer ${mockedToken}`)
+          .send(customOrder);
+        chai.expect(newOrder).to.have.status(200);
+      }
+
+      const res = await chai
+        .request(app)
+        .get('/orders/liquidity-provider')
+        .set('Authorization', `Bearer ${mockedToken}`);
+      chai.expect(res).to.have.status(200);
+
+      const offerIds = res.body.map((order) => order.offerId);
+
+      chai.expect(offerIds.every((offerId) => customOfferIds.includes(offerId)))
+        .to.be.true;
+
+      for (let i = 0; i < nbrOffersOrders; i++) {
+        const deleteOffer = await chai
+          .request(app)
+          .delete(`/offers/offerId-number${i}`)
+          .set('Authorization', `Bearer ${mockedToken}`);
+        chai.expect(deleteOffer).to.have.status(200);
+
+        const deleteOrder = await chai
+          .request(app)
+          .delete(`/orders/orderId-number${i}`)
+          .set('Authorization', `Bearer ${mockedToken}`);
+        chai.expect(deleteOrder).to.have.status(200);
+      }
+    });
+
+    it('Should show only orders corresponding to active offers', async function () {
+      this.timeout(50000);
+      const customOffer = { ...offer };
+      const customOrder = { ...order };
+      const nbrOffersOrders = 1;
+      for (let i = 0; i < nbrOffersOrders; i++) {
+        customOffer.offerId = `offerId-number${i}`;
+        // isActive est true pour les i pairs, false pour les i impairs
+        customOffer.isActive = i % 2 === 0;
+
+        const newOffer = await chai
+          .request(app)
+          .post('/offers')
+          .set('Authorization', `Bearer ${mockedToken}`)
+          .send(customOffer);
+        chai.expect(newOffer).to.have.status(200);
+
+        customOrder.orderId = `orderId-number${i}`;
+        customOrder.offerId = customOffer.offerId;
+
+        const newOrder = await chai
+          .request(app)
+          .post('/orders')
+          .set('Authorization', `Bearer ${mockedToken}`)
+          .send(customOrder);
+        chai.expect(newOrder).to.have.status(200);
+      }
+
+      const res = await chai
+        .request(app)
+        .get('/orders/liquidity-provider')
+        .set('Authorization', `Bearer ${mockedToken}`);
+      chai.expect(res).to.have.status(200);
+
+      const offers = await offerCollection
+        .find({ offerId: { $in: res.body.map((order) => order.offerId) } })
+        .toArray();
+
+      chai.expect(offers).to.be.an('array');
+      offers.forEach((offer) => {
+        chai.expect(offer.isActive).to.be.true;
+      });
+
+      for (let i = 0; i < nbrOffersOrders; i++) {
+        const deleteOffer = await chai
+          .request(app)
+          .delete(`/offers/offerId-number${i}`)
+          .set('Authorization', `Bearer ${mockedToken}`);
+        chai.expect(deleteOffer).to.have.status(200);
+
+        const deleteOrder = await chai
+          .request(app)
+          .delete(`/orders/orderId-number${i}`)
+          .set('Authorization', `Bearer ${mockedToken}`);
+        chai.expect(deleteOrder).to.have.status(200);
+      }
+    });
+
+    it('Should show only orders corresponding offers created by the user', async function () {
+      this.timeout(50000);
+      const customOffer = { ...offer };
+      const customOrder = { ...order };
+      const nbrOffersOrders = 1;
+      let userId = '';
+      for (let i = 0; i < nbrOffersOrders; i++) {
+        customOffer.offerId = `offerId-number${i}`;
+        // isActive est true pour les i pairs, false pour les i impairs
+        customOffer.isActive = i % 2 === 0;
+
+        const newOffer = await chai
+          .request(app)
+          .post('/offers')
+          .set('Authorization', `Bearer ${mockedToken}`)
+          .send(customOffer);
+        chai.expect(newOffer).to.have.status(200);
+
+        customOrder.orderId = `orderId-number${i}`;
+        customOrder.offerId = customOffer.offerId;
+
+        const newOrder = await chai
+          .request(app)
+          .post('/orders')
+          .set('Authorization', `Bearer ${mockedToken}`)
+          .send(customOrder);
+        chai.expect(newOrder).to.have.status(200);
+
+        if (i === 0) {
+          userId = (
+            await collection.findOne({
+              _id: new ObjectId(newOrder.body.insertedId),
+            })
+          ).userId;
+        }
+      }
+
+      const res = await chai
+        .request(app)
+        .get('/orders/liquidity-provider')
+        .set('Authorization', `Bearer ${mockedToken}`);
+      chai.expect(res).to.have.status(200);
+
+      const offers = await offerCollection
+        .find({ offerId: { $in: res.body.map((order) => order.offerId) } })
+        .toArray();
+
+      chai.expect(offers).to.be.an('array');
+      offers.forEach((offer) => {
+        chai.expect(offer.userId).to.equal(userId);
+      });
+
+      for (let i = 0; i < nbrOffersOrders; i++) {
+        const deleteOffer = await chai
+          .request(app)
+          .delete(`/offers/offerId-number${i}`)
+          .set('Authorization', `Bearer ${mockedToken}`);
+        chai.expect(deleteOffer).to.have.status(200);
+
+        const deleteOrder = await chai
+          .request(app)
+          .delete(`/orders/orderId-number${i}`)
+          .set('Authorization', `Bearer ${mockedToken}`);
+        chai.expect(deleteOrder).to.have.status(200);
+      }
     });
   });
 });
