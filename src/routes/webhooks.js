b5eed5a21eac1dc6212f2fa056544c7d3a6c294c
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-04-25 08:57:21 -0300

    update min price offer

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -3,11 +3,39 @@ import {
   updateStatusOfferValidator,
   updateChainIdOfferValidator,
   updateTokenOfferValidator,
+  updateMinPriceOfferValidator,
 } from '../validators/webhook.validator.js';
 import { validateResult } from '../utils/validators-utils.js';
 import getDBConnection from '../db/conn.js';
 
 const router = express.Router();
+
+/* This is a PUT request that updates min price offer. */
+router.put(
+  '/offer/min-price',
+  updateMinPriceOfferValidator,
+  async (req, res) => {
+    const validator = validateResult(req, res);
+    const collection = (await getDBConnection(req)).collection('offers');
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const offer = await collection.findOne({
+      offerId: req.body._idOffer,
+    });
+    if (offer)
+      return res.status(200).send(
+        await collection.updateOne(offer, {
+          $set: {
+            min: req.body._minPriceLimit ? req.body._minPriceLimit : offer.min,
+          },
+        })
+      );
+    res.status(400).send({
+      msg: 'Not found.',
+    });
+  }
+);
 
 /* This is a PUT request that updates token offer. */
 router.put('/offer/token', updateTokenOfferValidator, async (req, res) => {
--- src/validators/webhook.validator.js
+++ src/validators/webhook.validator.js
@@ -1,6 +1,50 @@
 import { body, param, query } from 'express-validator';
 import { validateFields } from '../utils/validators-utils.js';
 
+export const updateMinPriceOfferValidator = [
+  body('_grinderyChainId')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('must not be empty'),
+  body('_grinderyTransactionHash')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('must not be empty'),
+  body('_idOffer')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('must not be empty'),
+  body('_minPriceLimit')
+    .isString()
+    .withMessage('must be string value')
+    .notEmpty()
+    .withMessage('must not be empty'),
+  body().custom((value, { req }) => {
+    validateFields(
+      req.body,
+      [
+        '_grinderyChainId',
+        '_grinderyTransactionHash',
+        '_idOffer',
+        '_minPriceLimit',
+      ],
+      'body'
+    );
+    return true;
+  }),
+  query().custom((value, { req }) => {
+    validateFields(req.query, [], 'query');
+    return true;
+  }),
+  param().custom((value, { req }) => {
+    validateFields(req.params, [], 'params');
+    return true;
+  }),
+];
+
 export const updateChainIdOfferValidator = [
   body('_grinderyChainId')
     .isString()
