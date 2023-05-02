c5a5d530b21a546eff3b8c1b4d94563dd8365697
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-05-02 18:42:13 -0300

    add auth api key middleware

--- src/routes/webhooks.js
+++ src/routes/webhooks.js
@@ -11,12 +11,14 @@ import {
 import { validateResult } from '../utils/validators-utils.js';
 import { Database } from '../db/conn.js';
 import { dispatch } from '../utils/websocket-utils.js';
+import { authenticateApiKey } from '../utils/auth-utils.js';
 
 const router = express.Router();
 
 /* This is a PUT request that updates max price offer. */
 router.put(
   '/offer/max-price',
+  authenticateApiKey,
   updateMaxPriceOfferValidator,
   async (req, res) => {
     const validator = validateResult(req, res);
@@ -55,6 +57,7 @@ router.put(
 /* This is a PUT request that updates min price offer. */
 router.put(
   '/offer/min-price',
+  authenticateApiKey,
   updateMinPriceOfferValidator,
   async (req, res) => {
     const validator = validateResult(req, res);
@@ -91,173 +94,198 @@ router.put(
 );
 
 /* This is a PUT request that updates token offer. */
-router.put('/offer/token', updateTokenOfferValidator, async (req, res) => {
-  const db = await Database.getInstance(req);
-  const collection = db.collection('offers');
-  if (validator.length) {
-    return res.status(400).send(validator);
-  }
-  const offer = await collection.findOne({
-    offerId: req.body._idOffer,
-  });
-  if (offer) {
-    const response = await collection.updateOne(offer, {
-      $set: {
-        tokenAddress: req.body._token ? req.body._token : offer.tokenAddress,
-      },
+router.put(
+  '/offer/token',
+  authenticateApiKey,
+  updateTokenOfferValidator,
+  async (req, res) => {
+    const db = await Database.getInstance(req);
+    const collection = db.collection('offers');
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const offer = await collection.findOne({
+      offerId: req.body._idOffer,
     });
-    if (response.modifiedCount > 0)
-      dispatch({
-        method: 'update',
-        params: {
-          type: 'offer',
-          id: req.body._idOffer,
-          userId: offer.userId,
+    if (offer) {
+      const response = await collection.updateOne(offer, {
+        $set: {
+          tokenAddress: req.body._token ? req.body._token : offer.tokenAddress,
         },
-        id: req.body._idOffer,
       });
-    return res.status(200).send(response);
+      if (response.modifiedCount > 0)
+        dispatch({
+          method: 'update',
+          params: {
+            type: 'offer',
+            id: req.body._idOffer,
+            userId: offer.userId,
+          },
+          id: req.body._idOffer,
+        });
+      return res.status(200).send(response);
+    }
+    res.status(400).send({
+      msg: 'Not found.',
+    });
   }
-  res.status(400).send({
-    msg: 'Not found.',
-  });
-});
+);
 
 /* This is a PUT request that updates chain id offer. */
-router.put('/offer/chain', updateChainIdOfferValidator, async (req, res) => {
-  const db = await Database.getInstance(req);
-  const collection = db.collection('offers');
-  if (validator.length) {
-    return res.status(400).send(validator);
-  }
-  const offer = await collection.findOne({
-    offerId: req.body._idOffer,
-  });
-  if (offer) {
-    const response = await collection.updateOne(offer, {
-      $set: {
-        chainId: req.body._chainId ? req.body._chainId : offer.chainId,
-      },
+router.put(
+  '/offer/chain',
+  authenticateApiKey,
+  updateChainIdOfferValidator,
+  async (req, res) => {
+    const db = await Database.getInstance(req);
+    const collection = db.collection('offers');
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const offer = await collection.findOne({
+      offerId: req.body._idOffer,
     });
-    if (response.modifiedCount > 0)
-      dispatch({
-        method: 'update',
-        params: {
-          type: 'offer',
-          id: req.body._idOffer,
-          userId: offer.userId,
+    if (offer) {
+      const response = await collection.updateOne(offer, {
+        $set: {
+          chainId: req.body._chainId ? req.body._chainId : offer.chainId,
         },
-        id: req.body._idOffer,
       });
-    return res.status(200).send(response);
+      if (response.modifiedCount > 0)
+        dispatch({
+          method: 'update',
+          params: {
+            type: 'offer',
+            id: req.body._idOffer,
+            userId: offer.userId,
+          },
+          id: req.body._idOffer,
+        });
+      return res.status(200).send(response);
+    }
+    res.status(400).send({
+      msg: 'Not found.',
+    });
   }
-  res.status(400).send({
-    msg: 'Not found.',
-  });
-});
+);
 
 /* This is a PUT request that updates status offer. */
-router.put('/offer/status', updateStatusOfferValidator, async (req, res) => {
-  const db = await Database.getInstance(req);
-  const collection = db.collection('offers');
-  if (validator.length) {
-    return res.status(400).send(validator);
-  }
-  const offer = await collection.findOne({
-    offerId: req.body._idOffer,
-  });
-  if (offer) {
-    const response = await collection.updateOne(offer, {
-      $set: {
-        isActive:
-          req.body._isActive === undefined
-            ? offer.isActive
-            : req.body._isActive,
-      },
+router.put(
+  '/offer/status',
+  authenticateApiKey,
+  updateStatusOfferValidator,
+  async (req, res) => {
+    const db = await Database.getInstance(req);
+    const collection = db.collection('offers');
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const offer = await collection.findOne({
+      offerId: req.body._idOffer,
     });
-    if (response.modifiedCount > 0)
-      dispatch({
-        method: 'update',
-        params: {
-          type: 'offer',
-          id: req.body._idOffer,
-          userId: offer.userId,
+    if (offer) {
+      const response = await collection.updateOne(offer, {
+        $set: {
+          isActive:
+            req.body._isActive === undefined
+              ? offer.isActive
+              : req.body._isActive,
         },
-        id: req.body._idOffer,
       });
-    return res.status(200).send(response);
+      if (response.modifiedCount > 0)
+        dispatch({
+          method: 'update',
+          params: {
+            type: 'offer',
+            id: req.body._idOffer,
+            userId: offer.userId,
+          },
+          id: req.body._idOffer,
+        });
+      return res.status(200).send(response);
+    }
+    res.status(404).send({
+      msg: 'Not found.',
+    });
   }
-  res.status(404).send({
-    msg: 'Not found.',
-  });
-});
+);
 
 /* This is a PUT request that updates the offer id. */
-router.put('/offer', updateOfferValidator, async (req, res) => {
-  const db = await Database.getInstance(req);
-  const collection = db.collection('offers');
-  if (validator.length) {
-    return res.status(400).send(validator);
-  }
-  const offer = await collection.findOne({
-    hash: req.body._grinderyTransactionHash,
-  });
-  if (offer) {
-    const response = await collection.updateOne(offer, {
-      $set: {
-        offerId: req.body._idOffer,
-        status: 'sucess',
-      },
+router.put(
+  '/offer',
+  authenticateApiKey,
+  updateOfferValidator,
+  async (req, res) => {
+    const db = await Database.getInstance(req);
+    const collection = db.collection('offers');
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const offer = await collection.findOne({
+      hash: req.body._grinderyTransactionHash,
     });
-    if (response.modifiedCount > 0)
-      dispatch({
-        method: 'update',
-        params: {
-          type: 'offer',
-          id: req.body._idOffer,
-          userId: offer.userId,
+    if (offer) {
+      const response = await collection.updateOne(offer, {
+        $set: {
+          offerId: req.body._idOffer,
+          status: 'sucess',
         },
-        id: req.body._idOffer,
       });
-    return res.status(200).send(response);
+      if (response.modifiedCount > 0)
+        dispatch({
+          method: 'update',
+          params: {
+            type: 'offer',
+            id: req.body._idOffer,
+            userId: offer.userId,
+          },
+          id: req.body._idOffer,
+        });
+      return res.status(200).send(response);
+    }
+    res.status(404).send({
+      msg: 'Not found.',
+    });
   }
-  res.status(404).send({
-    msg: 'Not found.',
-  });
-});
+);
 
 /* This is a PUT request that updates offer trade. */
-router.put('/order', updateOrderValidator, async (req, res) => {
-  const db = await Database.getInstance(req);
-  const collection = db.collection('orders');
-  if (validator.length) {
-    return res.status(400).send(validator);
-  }
-  const order = await collection.findOne({
-    hash: req.body._grinderyTransactionHash,
-  });
-  if (order) {
-    const response = await collection.updateOne(order, {
-      $set: {
-        orderId: req.body._idTrade,
-        status: 'sucess',
-      },
+router.put(
+  '/order',
+  authenticateApiKey,
+  updateOrderValidator,
+  async (req, res) => {
+    const db = await Database.getInstance(req);
+    const collection = db.collection('orders');
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+    const order = await collection.findOne({
+      hash: req.body._grinderyTransactionHash,
     });
-    if (response.modifiedCount > 0)
-      dispatch({
-        method: 'update',
-        params: {
-          type: 'order',
-          id: req.body._idTrade,
-          userId: order.userId,
+    if (order) {
+      const response = await collection.updateOne(order, {
+        $set: {
+          orderId: req.body._idTrade,
+          status: 'sucess',
         },
-        id: req.body._idTrade,
       });
-    return res.status(200).send(response);
+      if (response.modifiedCount > 0)
+        dispatch({
+          method: 'update',
+          params: {
+            type: 'order',
+            id: req.body._idTrade,
+            userId: order.userId,
+          },
+          id: req.body._idTrade,
+        });
+      return res.status(200).send(response);
+    }
+    res.status(404).send({
+      msg: 'Not found.',
+    });
   }
-  res.status(404).send({
-    msg: 'Not found.',
-  });
-});
+);
 
 export default router;
