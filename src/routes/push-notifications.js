8f6df193a11163877a8a85fc5a2f8cd9679b21a1
Mauricio Junior <mauriciojunior@Mauricios-MacBook-Air.local>
2023-05-09 17:26:44 -0300

    add push notification routes

---
+++ src/routes/push-notifications.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import isRequired from '../utils/auth-utils.js';
+import { Database } from '../db/conn.js';
+import { validateResult } from '../utils/validators-utils.js';
+import {
+  getNotificationTokenValidator,
+  updateNotificationTokenValidator,
+} from '../validators/push-notifications.validator.js';
+
+const router = express.Router();
+
+router.put(
+  '/',
+  isRequired,
+  updateNotificationTokenValidator,
+  async (req, res) => {
+    const validator = validateResult(req, res);
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+
+    const db = await Database.getInstance(req);
+    const collection = db.collection('notification-tokens');
+
+    res.status(200).send(
+      await collection.updateOne(
+        {
+          userId: res.locals.userId,
+        },
+        {
+          $set: { token: req.body.token },
+        },
+        { upsert: true }
+      )
+    );
+  }
+);
+
+router.delete(
+  '/:token',
+  isRequired,
+  getNotificationTokenValidator,
+  async (req, res) => {
+    const validator = validateResult(req, res);
+    if (validator.length) {
+      return res.status(400).send(validator);
+    }
+
+    const db = await Database.getInstance(req);
+    const collection = db.collection('notification-tokens');
+
+    const token = await collection.findOne({
+      token: req.params.token,
+      userId: res.locals.userId,
+    });
+
+    if (token) {
+      res.status(200).send(await collection.deleteOne(token));
+    } else {
+      res.status(404).send({
+        msg: 'No notification token found',
+      });
+    }
+  }
+);
+
+export default router;
